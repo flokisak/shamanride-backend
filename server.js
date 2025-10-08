d9a4a97c094067993cf7c39fecd0af0fc6deddfa
Misha <floki@jobshaman.cz>
2025-10-08 14:22:22 +0200

    update serveru

--- server.js
+++ server.js
@@ -3,6 +3,18 @@ const app = express();
 
 app.use(express.json());
 
+// Enable CORS
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
+
 app.post('/api/config', (req, res) => {
   const { server, username, password } = req.body;
   // In production, perhaps store in env or database
